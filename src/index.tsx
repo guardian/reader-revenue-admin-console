2d20da74a97db2127a5bb327837b3e1f95bf02ea
Joseph Smith <joseph.smith@guardian.co.uk>
2018-08-29 12:24:25 +0100

    Edit amounts and log updated amounts to the console

--- src/index.tsx
+++ src/index.tsx
@@ -13,72 +13,107 @@ import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 
 // form
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import Switch from '@material-ui/core/Switch';
+// import FormLabel from '@material-ui/core/FormLabel';
+// import FormControl from '@material-ui/core/FormControl';
+// import FormGroup from '@material-ui/core/FormGroup';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import FormHelperText from '@material-ui/core/FormHelperText';
+// import Switch from '@material-ui/core/Switch';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 
-class SwitchesGroup extends React.Component {
-  state = {
-    gilad: true,
-    jason: false,
-    antoine: true,
-  };
 
-  handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ [name]: event.target.checked });
-  };
+// class SwitchesGroup extends React.Component {
+//   state = {
+//     gilad: true,
+//     jason: false,
+//     antoine: true,
+//   };
 
-  render() {
-    return (
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Assign responsibility</FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={this.state.gilad}
-                onChange={this.handleChange('gilad')}
-                value="gilad"
-              />
-            }
-            label="Gilad Gray"
-          />
-          <FormControlLabel
-            control={
-              <Switch
-                checked={this.state.jason}
-                onChange={this.handleChange('jason')}
-                value="jason"
-              />
-            }
-            label="Jason Killian"
-          />
-          <FormControlLabel
-            control={
-              <Switch
-                checked={this.state.antoine}
-                onChange={this.handleChange('antoine')}
-                value="antoine"
-              />
-            }
-            label="Antoine Llorca"
-          />
-        </FormGroup>
-        <FormHelperText>Be careful</FormHelperText>
-      </FormControl>
-    );
-  }
-}
+//   handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+//     this.setState({ [name]: event.target.checked });
+//   };
+
+//   render() {
+//     return (
+//       <FormControl component="fieldset">
+//         <FormLabel component="legend">Assign responsibility</FormLabel>
+//         <FormGroup>
+//           <FormControlLabel
+//             control={
+//               <Switch
+//                 checked={this.state.gilad}
+//                 onChange={this.handleChange('gilad')}
+//                 value="gilad"
+//               />
+//             }
+//             label="Gilad Gray"
+//           />
+//           <FormControlLabel
+//             control={
+//               <Switch
+//                 checked={this.state.jason}
+//                 onChange={this.handleChange('jason')}
+//                 value="jason"
+//               />
+//             }
+//             label="Jason Killian"
+//           />
+//           <FormControlLabel
+//             control={
+//               <Switch
+//                 checked={this.state.antoine}
+//                 onChange={this.handleChange('antoine')}
+//                 value="antoine"
+//               />
+//             }
+//             label="Antoine Llorca"
+//           />
+//         </FormGroup>
+//         <FormHelperText>Be careful</FormHelperText>
+//       </FormControl>
+//     );
+//   }
+// }
 
 const rootStyle = css`
   flex-grow: 1;
 `;
 
-function SimpleAppBar() {
+function Amounts({amounts}: {amounts: Object}) {
+  const oneOffAmounts = amounts['ONE_OFF'];
+  return (<div>
+    {Object.keys(oneOffAmounts).map(countryGroup =>
+      <div className="countryGroup">
+        <Typography variant="title" color="inherit">
+          {countryGroup}
+        </Typography>
+        {oneOffAmounts[countryGroup].map((amount: {value: string}) => 
+          <TextField defaultValue={amount.value} />
+        )}
+      </div>
+    )}
+  </div>);
+}
+
+const save = (originalAmounts: Object) => () => {
+  let data = {...originalAmounts};
+  Array.from(document.getElementsByClassName('countryGroup')).forEach(el => {
+    const h2 = el.querySelector('h2');
+    const countryGroup = h2 ? h2.innerHTML : 'h2 not found!';
+    const newAmounts: Array<{value: string, spoken: string}> = [];
+    Array.from(el.querySelectorAll('input')).forEach(input => {
+      newAmounts.push({value: input.value, spoken: 'fixme'});
+    });
+    data['ONE_OFF'][countryGroup] = newAmounts;
+  })
+
+  console.log('saving:');
+  console.log(data);
+}
+
+function SimpleAppBar({amounts}: {amounts: Object}) {
   return (
     <div className={rootStyle}>
       <AppBar position="static" color="default">
@@ -88,14 +123,18 @@ function SimpleAppBar() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <SwitchesGroup />
+      <Amounts amounts={amounts} />
+      <Button size="large" color="primary" onClick={save(amounts)}>Save</Button>
     </div>
   );
 }
 
-
-ReactDOM.render(
-  <SimpleAppBar></SimpleAppBar>,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+fetch('http://joseph-smith-support-frontend-config.s3-website-eu-west-1.amazonaws.com/amounts.json')
+  .then(resp => resp.json())
+  .then(amounts => {
+    ReactDOM.render(
+      <SimpleAppBar amounts={amounts}></SimpleAppBar>,
+      document.getElementById('root') as HTMLElement
+    );
+    registerServiceWorker();
+  });
