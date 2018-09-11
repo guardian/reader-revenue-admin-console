62cec937c3d7cb6a71c6ff7822dd33b186e16305
guy.dawson <guy.dawson@theguardian.com>
2018-09-11 13:44:23 +0100

    get rid of commented out code

--- src/index.tsx
+++ src/index.tsx
@@ -5,77 +5,13 @@ import 'typeface-roboto';
 import registerServiceWorker from './registerServiceWorker';
 import { css } from 'react-emotion';
 
-// typography
 import Typography from '@material-ui/core/Typography';
 
-// app bar
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 
-// form
-// import FormLabel from '@material-ui/core/FormLabel';
-// import FormControl from '@material-ui/core/FormControl';
-// import FormGroup from '@material-ui/core/FormGroup';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import FormHelperText from '@material-ui/core/FormHelperText';
-// import Switch from '@material-ui/core/Switch';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
-
-
-// class SwitchesGroup extends React.Component {
-//   state = {
-//     gilad: true,
-//     jason: false,
-//     antoine: true,
-//   };
-
-//   handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-//     this.setState({ [name]: event.target.checked });
-//   };
-
-//   render() {
-//     return (
-//       <FormControl component="fieldset">
-//         <FormLabel component="legend">Assign responsibility</FormLabel>
-//         <FormGroup>
-//           <FormControlLabel
-//             control={
-//               <Switch
-//                 checked={this.state.gilad}
-//                 onChange={this.handleChange('gilad')}
-//                 value="gilad"
-//               />
-//             }
-//             label="Gilad Gray"
-//           />
-//           <FormControlLabel
-//             control={
-//               <Switch
-//                 checked={this.state.jason}
-//                 onChange={this.handleChange('jason')}
-//                 value="jason"
-//               />
-//             }
-//             label="Jason Killian"
-//           />
-//           <FormControlLabel
-//             control={
-//               <Switch
-//                 checked={this.state.antoine}
-//                 onChange={this.handleChange('antoine')}
-//                 value="antoine"
-//               />
-//             }
-//             label="Antoine Llorca"
-//           />
-//         </FormGroup>
-//         <FormHelperText>Be careful</FormHelperText>
-//       </FormControl>
-//     );
-//   }
-// }
 
 const rootStyle = css`
   flex-grow: 1;
