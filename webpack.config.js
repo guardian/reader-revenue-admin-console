9cc1acb2780d1a4e009065988c8657949db588bf
guy.dawson <guy.dawson@theguardian.com>
2018-09-12 15:07:47 +0100

    init webback file

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+path = require('path')
+
+
+const server = {
+
+
+}
+
+const client = {
+    entry: './client/index',
+    output: {
+
+    }
+
+}
+
+module.exports = {
+    client: client,
+    server: server,
+}
